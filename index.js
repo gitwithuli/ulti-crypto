c263492acac695bb55ffa32ef13526a818feda19
Ulas <uli.will.code@protonmail.com>
2020-08-02 15:42:21 +0300

    continue

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -18,9 +20,18 @@ app.post('/api/mine', (req, res) => {
     
     blockchain.addBlock({ data });
 
+    pubsub.broadcastChain();
+
     res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
 
