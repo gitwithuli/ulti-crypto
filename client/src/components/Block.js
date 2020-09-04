c1f5bcc13f57bc40953d4d4fd1dd1968beacef6c
Ulas <uli.will.code@protonmail.com>
2020-09-04 20:49:05 +0300

    added Transactions

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
     state = { displayTransaction: false };
@@ -17,10 +18,40 @@ class Block extends Component {
         `${stringifiedData.substring(0, 35)}...` :
         stringifiedData;
 
+        if (this.state.displayTransaction) {
+            return (
+                <div>
+                    {
+                        data.map(transaction => (
+                            <div key={transaction.id}>
+                                <hr />
+                                <Transaction transaction={transaction} />
+                            </div>
+                        ))
+                    }
+                    <br />
+                    <Button 
+                    bsStyle="danger" 
+                    bsSize="small" 
+                    onClick={this.toggleTransaction}
+                >
+                    Show Less
+                </Button>
+
+                </div>
+            )
+        }
+
         return (
             <div>
                 <div>Data: {dataDisplay}</div>
-                <Button>Show More</Button>
+                <Button 
+                bsStyle="danger" 
+                bsSize="small" 
+                onClick={this.toggleTransaction}
+                >
+                    Show More
+                </Button>
             </div>
         );
     }
