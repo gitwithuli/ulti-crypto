dbca2f59d819da70c792feb1100151a19230d137
Ulas <uli.will.code@protonmail.com>
2020-09-04 12:11:54 +0300

    added wallet information to the frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App'
 
 render(
-<div>UltiCrypto in React! Live Update</div>, 
+<App/>, 
 document.getElementById('root')
 );
